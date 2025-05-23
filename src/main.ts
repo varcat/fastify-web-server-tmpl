394578022ad1cb568fe235e6bf9f91da9d8e4825
shun <810100157@qq.com>
2025-05-23 17:19:25 +0800

    feat: 添加开发脚本并增强API文档功能
    - 在package.json中添加`dev`脚本以支持开发环境热重载
    - 添加`nodemon`和`ts-node`依赖，用于开发环境
    - 增强`main.ts`中的API文档功能，集成Swagger和Swagger UI
    - 更新`tsconfig.json`以支持更严格的类型检查和模块解析

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,95 @@
-import fastify from 'fastify'
+import fastify from "fastify";
+import { Type, TypeBoxTypeProvider } from "@fastify/type-provider-typebox";
+import swagger from "@fastify/swagger";
+import swaggerUi from "@fastify/swagger-ui";
 
-const server = fastify()
+async function main() {
+  const server = fastify().withTypeProvider<TypeBoxTypeProvider>();
 
-server.get('/ping', async (request, reply) => {
-  return 'pong\n'
-})
+  await server.register(swagger, {
+    openapi: {
+      openapi: "3.0.0",
+      info: {
+        title: "Test swagger",
+        description: "Testing the Fastify swagger API",
+        version: "1.0.0",
+      },
+      servers: [
+        {
+          url: "http://localhost:3000",
+          description: "Development server",
+        },
+        {
+          url: "http://192.168.31.2:3000",
+          description: "test server",
+        },
+      ],
+    },
+  });
+  await server.register(swaggerUi, {
+    routePrefix: "/documentation",
+    uiConfig: {
+      filter: true,
+      docExpansion: "list",
+      deepLinking: true,
+    },
+    uiHooks: {
+      onRequest: function (request, reply, next) {
+        next();
+      },
+      preHandler: function (request, reply, next) {
+        next();
+      },
+    },
+    staticCSP: true,
+    transformStaticCSP: (header) => header,
+    transformSpecification: (swaggerObject, request, reply) => {
+      return swaggerObject;
+    },
+    transformSpecificationClone: true,
+  });
 
-server.listen({ port: 8080 }, (err, address) => {
-  if (err) {
-    console.error(err)
-    process.exit(1)
-  }
-  console.log(`Server listening at ${address}`)
-})
+  server.get(
+    "/ping",
+    {
+      schema: {
+        description: "ping测试文案description",
+        tags: ["user"],
+        summary: "ping测试文案summary",
+      },
+    },
+    async (request, reply) => {
+      return "pong\n";
+    }
+  );
+
+  server.post(
+    "/",
+    {
+      schema: {
+        body: Type.Object({
+          x: Type.String(),
+          y: Type.Number(),
+          z: Type.Boolean(),
+        }),
+      },
+    },
+    (req) => {
+      // The `x`, `y`, `z` types are automatically inferred
+      const { x, y, z } = req.body;
+      return { x, y, z };
+    }
+  );
+
+  server.listen({ port: 8080 }, (err, address) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    console.log(`Server listening at ${address}`);
+  });
+
+  await server.ready();
+}
+
+main();
